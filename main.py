71f091f746daef49fcc697a16d2f4d1759b217a7
Ramiro Medina <64783088+ramedina86@users.noreply.github.com>
2025-10-03 23:51:24 +0200

    feat: more

--- main.py
+++ main.py
@@ -5,5 +5,8 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World 2"}
+    return {"message": "Hello World 3"}
 
+@app.get("/a1")
+async def a1():
+    return {"message": "This is a1"}
