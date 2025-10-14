dac74352567db83e6ae83a80331af854ceace75c
Ramiro Medina <64783088+ramedina86@users.noreply.github.com>
2025-10-14 20:52:13 +0200

    update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello again World from the feat-emoji-message branch, which has an emoji: 🌎"}
+    return {"message": "Hello k World from the feat-emoji-message branch, which has an emoji: 🌎"}
 
 @app.get("/secondary")
 async def root():
